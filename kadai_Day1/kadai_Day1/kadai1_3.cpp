baba24f4ecc6f1bf04ed935521402b5f7d13a7d4
KyusokLee <kyusok1@naver.com>
2022-05-17 10:39:11 +0900

    実験 Day1 _ completed

--- kadai_Day1/kadai_Day1/kadai1_3.cpp
+++ kadai_Day1/kadai_Day1/kadai1_3.cpp
@@ -16,13 +16,16 @@
 
 using namespace std;
 
-int main_1_3() {
-    ofstream kadai_1("kadai_1(lamda:2).csv");
+int main1_3() {
+    ofstream kadai_1("kadai_1(lamda:10.0).csv");
     double rndUni[1001] = {0};
     double rndExp[1001] = {0};
     double sumAverage = 0.0;
     double sumUnia_b = 0.0;
     double sumExp = 0.0;
+    double lamda;
+    cout << "lamda: ";
+    cin >> lamda;
     
     int upperBound = 1000;
     srand(20141106); // シード: 学習番号
@@ -30,7 +33,7 @@ int main_1_3() {
     for (int i = 0; i < upperBound; i++) {
         sumAverage += rnd_uni(1, 2, rnd_val1(rand()));
         rndUni[i] = rnd_uni(1, 2, rnd_val1(rand()));
-        rndExp[i] = rnd_exp(2.0, rnd_val1(rand()));
+        rndExp[i] = rnd_exp(lamda, rnd_val1(rand()));
         sumUnia_b += rndUni[i];
         sumExp += rndExp[i];
         kadai_1 << i << "," <<rndUni[i] << "," << rndExp[i] << endl;
@@ -46,7 +49,7 @@ int main_1_3() {
     sumExp = round(sumExp * 100000) / 100000;
     cout << "求めた平均値:" << sumAverage << endl;
     cout << "一様分布乱数の平均値(a: 1, b: 2): " << sumUnia_b << endl;
-    cout << "指数分布乱数の平均値(lamda: 0.5): " << sumExp << endl;
+    cout << "指数分布乱数の平均値(lamda:" << lamda << "): " << sumExp << endl;
     
     return 0;
 }
--- kadai_Day1/kadai_Day1/kadai_2_2_2.cpp
+++ kadai_Day1/kadai_Day1/kadai_2_2_2.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <random>
 #include <math.h>
 #include <ctime>
@@ -39,38 +40,48 @@ public:
 };
 
 // 課題2-2_2 (N個のサイコロの場合)(試行回数: 10000回)
+// N = 2 ~ 10までチェックした
+// 中心極限利用
 // ファイル入出力あり
 int main() {
-    ofstream kadai2_2_2("kadai2_2_2.csv");
-    kadai2_2_2 << "i番目" << "," << "サイコロの出る目の和" << endl;
+    FILE *kadai2_DICE_N = fopen("kadai2_2DICE_N", "wb");
+    FILE *kadai2_frequency = fopen("kadai2_frequency", "wb");
     
-    dice a;
-    dice b;
+    dice diceN;
     srand(20141106);
     
-    double averagesum = 0;
-    double sumA = 0;
-    double sumB = 0;
-    double stdDeviation = 0;
-    int startcases = 10000;
+    int diceCount = 0; //サイコロの個数
+    int count = 0; //試行回数
+    int diceSum = 0; // n個のサイコロの和
+    int startCases = 10000;
+    int diceSumCount[61] = {0}; // 9個のサイコロの和の頻度を格納する配列 (0)に初期化
+    double tempSum = 0.0; //２つのサイコロの和の合算 (試行回数をある値に固定したとき)
+    double averageSum = 0.0; //２つのサイコロの和の平均 (試行回数をある値に固定したとき)
     
-    for (int i = 0; i < startcases; i++) {
-        a.Roll();
-        b.Roll();
-        kadai2_2_2 << i << "," << a.value + b.value << endl;
-        sumA += a.value;
-        sumB += b.value;
+    for (diceCount = 2; diceCount <= 10; diceCount++) {
+        for (count = 1; count <= startCases; count++) {
+            tempSum = 0;
+            for (int i = 1; i <= count; i++) {
+                diceSum = 0;
+                for (int j = 1; j <= diceCount; j++) {
+                    diceN.Roll();
+                    diceSum += diceN.value;
+                }
+                diceSumCount[diceSum] += 1;
+                tempSum += diceSum;
+            }
+            averageSum = tempSum / count; //平均値を計算
+            fprintf(kadai2_DICE_N, "%d, %d, %d, %f\n", diceCount, count, diceSum, averageSum);
+            //サイコロの個数、試行回数、n個のサイコロの和、平均値を出力
+        }
+        
+        for (int k = 0; k < 61; k++) {
+            fprintf(kadai2_frequency, "%d, %d\n", k, diceSumCount[k]);
+            diceSumCount[k] = 0;
+        }
     }
-    
-    kadai2_2_2.close();
-    
-    sumA /= startcases;
-    sumB /= startcases;
-    averagesum = (sumA + sumB) / 2;
-    stdDeviation = sqrt((pow(sumA - averagesum, 2) + pow(sumB - averagesum, 2)) / 2);
-    
-    cout << "サイコロ2個の10回試行の平均値（期待値）: " << averagesum << endl;
-    cout << "その標準偏差の平均値: " << stdDeviation << endl;
+    fclose(kadai2_DICE_N);
+    fclose(kadai2_frequency);
     
     return 0;
 }
